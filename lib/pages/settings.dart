0dccec51154532bdefaf293041ae438d19e06521
Corey Mitchell <corey.waitforit.mitchell@gmail.com>
2026-02-15 17:23:28 -0600

    Created stand in pages with under construction widgets for the settings and event pages.

---
+++ lib/pages/settings.dart
@@ -0,0 +1,33 @@
+import 'package:accountabill/widgets/under_construction.dart';
+import 'package:flutter/material.dart';
+
+/// User settings page
+///
+/// 🚧 Currently under construction 🚧
+///
+/// This is where the user will be able to see and
+/// edit their selected charity, payment instrument and
+/// any additional settings.
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  /// Page scaffold
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: _appBar(context), body: _buildUI(context));
+  }
+
+  /// Page app bar widget
+  PreferredSizeWidget _appBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.primaryFixed,
+      title: Text("User Settings"),
+      centerTitle: true,
+    );
+  }
+
+  /// Page UI main widget
+  Widget _buildUI(BuildContext context) {
+    return UnderConstruction();
+  }
+}
