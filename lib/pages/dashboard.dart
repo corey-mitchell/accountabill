d5c5ed0289f7b9db369dc94a2e776f1e1b10d07e
Corey Mitchell <corey.waitforit.mitchell@gmail.com>
2026-02-15 15:49:11 -0600

    Moved my Dashboard page into a pages folder, along with the initial flutter page for reference. Created a widgets folder for holding standalone components. Created a completion rate component with a simple pie chart, using fl_chart. Created a money donated component for displaying a value to the user. Updated dashboard layout to align with my wireframes.

---
+++ lib/pages/dashboard.dart
@@ -0,0 +1,90 @@
+import 'package:accountabill/widgets/completion_rate.dart';
+import 'package:accountabill/widgets/money_donated.dart';
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key, required this.userName});
+
+  final String userName;
+
+  // Return page scaffold
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: _appBar(context), body: _buildUI(context));
+  }
+
+  // Page app bar
+  PreferredSizeWidget _appBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.primaryFixed,
+      title: Text("Welcome, $userName"),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.settings),
+          onPressed: () {
+            print('Route to settings page');
+          },
+        ),
+      ],
+    );
+  }
+
+  // Page UI
+  Widget _buildUI(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        spacing: 32,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [CompletionRate(), MoneyDonated()],
+          ),
+          Container(
+            width: double.infinity, // consume full width
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Theme.of(context).colorScheme.inverseSurface,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            child: Column(
+              spacing: 16,
+              children: [
+                Text(
+                  "Words of encouragement:",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipisicing elif.",
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+          Spacer(), // Push button to the bottom
+          Padding(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).padding.bottom,
+            ),
+            child: SizedBox(
+              width: double.infinity,
+              height: 48,
+              child: ElevatedButton(
+                onPressed: () {
+                  print('Route to events page');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                  foregroundColor: Colors.white,
+                ),
+                child: Text("Schedule an event"),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:accountabill/main.dart';
+import 'package:accountabill/pages/flutter_init.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
