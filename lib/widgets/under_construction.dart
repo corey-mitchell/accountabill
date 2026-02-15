5f74e9ce4f377ac3e5946eb119fcf2dc459cb4fd
Corey Mitchell <corey.waitforit.mitchell@gmail.com>
2026-02-15 17:22:18 -0600

    Updated under construction widget to consume the entirety of it's parent spacing. It will then center itself in that space, taking into account the device's bottom safe area.

--- lib/widgets/under_construction.dart
+++ lib/widgets/under_construction.dart
@@ -8,24 +8,37 @@ class UnderConstruction extends StatelessWidget {
   /// Show a large construction icon and a message to the user
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        spacing: 32,
-        children: [
-          Center(
-            widthFactor: double.infinity,
-            child: Icon(
-              Icons.construction,
-              size: 200,
-              color: Theme.of(context).colorScheme.inverseSurface,
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.fromLTRB(
+        16,
+        16,
+        16,
+        MediaQuery.of(context).padding.bottom,
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment:
+              MainAxisAlignment.center, // Vertically center the content
+          crossAxisAlignment:
+              CrossAxisAlignment.center, // Horizontally center the content
+          spacing: 32,
+          children: [
+            Center(
+              widthFactor: double.infinity,
+              child: Icon(
+                Icons.construction,
+                size: 200,
+                color: Theme.of(context).colorScheme.inverseSurface,
+              ),
             ),
-          ),
-          Text(
-            "This page is currently under construction",
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-        ],
+            Text(
+              "This page is currently under construction",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
     );
   }
