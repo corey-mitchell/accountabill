14e3143d79847f99d3ff43a815f4cceb500b1365
Corey Mitchell <corey.waitforit.mitchell@gmail.com>
2026-02-15 17:23:28 -0600

    Created stand in pages with under construction widgets for the settings and event pages.

---
+++ lib/pages/events.dart
@@ -0,0 +1,32 @@
+import 'package:accountabill/widgets/under_construction.dart';
+import 'package:flutter/material.dart';
+
+/// User events page
+///
+/// 🚧 Currently under construction 🚧
+///
+/// This is where the user will be able to see and
+/// edit their events on a calendar component
+class EventsPage extends StatelessWidget {
+  const EventsPage({super.key});
+
+  /// Page scaffold
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: _appBar(context), body: _buildUI(context));
+  }
+
+  /// Page app bar widget
+  PreferredSizeWidget _appBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.primaryFixed,
+      title: Text("Events"),
+      centerTitle: true,
+    );
+  }
+
+  /// Page UI main widget
+  Widget _buildUI(BuildContext context) {
+    return UnderConstruction();
+  }
+}
